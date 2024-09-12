43df2ab179ff70a20fbf0c0116d077bf361ab30f
vitorHugo87 <vitorpaduan87@gmail.com>
2024-09-12 11:50:56 -0300

    Consulta com varios criterios

--- src/main/java/com/vitorpaduan/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/vitorpaduan/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.vitorpaduan.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -13,6 +14,9 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
 	@Query("{ 'title': { $regex: ?0, $options: 'i'} }")
 	List<Post> findByTitle(String text);
-	
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
+
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSerach(String text, Date minDate, Date maxDate);
 }
